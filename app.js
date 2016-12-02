8dd974661af54cda043ec9385b93dd07e8fed8b4
Robert <rll@bu.edu>
2016-12-02 15:07:13 +0000

    Most recent update

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ var app = express();
 var port = process.env.PORT || 3500;
 
 app.get('/', function(req, res){
-    res.end("Hello World!");
-})
+    
+    var information = {
+        "ipaddress": req.headers["x-forwarded-for"],
+        "language": req.headers["accept-language"].substring(0,5),
+        "software": req.headers["user-agent"]
+    }
+    
+    res.end(JSON.stringify(information, null, 3));
+    
+});
 
 app.listen(port, function() {
     console.log("Server listening on port " + port);
